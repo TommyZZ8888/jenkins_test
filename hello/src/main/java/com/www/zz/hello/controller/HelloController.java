06d93ce4d31a9c8c7be7f911190827750f28aa6f
卫刚 张 <1950442495@qq.com>
2025-07-03 12:18:39 +0800

    init

--- hello/src/main/java/com/www/zz/hello/controller/HelloController.java
+++ hello/src/main/java/com/www/zz/hello/controller/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("")
 	public String hello() {
-        return "Hello World!";
+        return "Hello jenkins!";
 	}
 }
